d3eb5ccadc1ff994f152f17cb24fe1e6bab6b6d7
DaniGil2 <86826366+DaniGil2@users.noreply.github.com>
2022-02-14 04:29:55 +0100

    Update baseline_methods.py

--- lib/baseline_methods.py
+++ lib/baseline_methods.py
@@ -5,6 +5,11 @@ from keras.metrics import top_k_categorical_accuracy
 from keras.backend import clear_session
 from keras.models import Sequential
 from keras.layers import Dense
+from tqdm.notebook import tqdm
+from keras.models import model_from_json
+import functools
+top2_accuracy = functools.partial(top_k_categorical_accuracy, k=2)
+top2_accuracy.__name__ = 'TOP2_ACC' #Defining TOP-2 accuracy 
 #Computations 
 import numpy as np
 #Tables
@@ -101,6 +106,54 @@ def MNB(x_train,y_train,x_test,y_test,dataset):
     
     
     
-#def    
+def NN(x_train,y_train,x_test,y_test,dictionary,dataset):
+    top1_acc_list = []
+    top2_acc_list = []
+    n_runs = 30
+
+    for i in tqdm(range(n_runs)):
+        clear_session()
+        #Neural Architecture Definition
+        model = Sequential()
+        model.add(Dense(512, activation='tanh', input_shape=(len(dictionary),)))
+        model.add(Dense(256, activation='tanh'))
+        if dataset=="wiki":
+            l = len(new_doc_utils.ALL_TOPICS)
+        else:
+            l = len(new_doc_utils.ARXIV_WIKI_TOPICS)
+        model.add(Dense(l, activation='softmax'))
+
+
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy', top2_accuracy])
+        hist = model.fit(x_train, y_train, epochs=5,verbose=0)
+        model_top1_acc = model.evaluate(x_test, y_test, verbose=0)[1]
+        model_top2_acc = model.evaluate(x_test, y_test, verbose=0)[2]
+            
+        if top2_acc_list and model_top2_acc >= max(top2_acc_list):
+            #save best model to date
+            model_json = model.to_json()
+            with open("model.json", "w") as f:
+                f.write(model_json)
+            model.save_weights("model.h5")
+        
+        top1_acc_list.append(model_top1_acc)
+        top2_acc_list.append(model_top2_acc)
+
+    json_f = open('model.json', 'r')
+    best_model_json = json_f.read()
+    json_f.close()
+    best_model = model_from_json(best_model_json)
+    best_model.load_weights("model.h5")#loading weights
+
+    # evaluate loaded model on test data
+    best_model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy', top2_accuracy])
+
+    predict_x=model.predict(x_test) 
+    predictions=np.argmax(predict_x,axis=1)
+
+    print("TOP-1 acc.:",best_model.evaluate(x_test, y_test, verbose=0)[1])
+    print("TOP-2 acc.:",best_model.evaluate(x_test, y_test, verbose=0)[2])
+
+    new_doc_utils.plotConfMatrix(y_test, predictions, model = "NN", dataset_type=dataset)
     
     
