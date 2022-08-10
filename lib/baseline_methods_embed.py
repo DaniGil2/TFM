28b59ed35ceed2c5075c7fa34b38d07eb134a9d0
DaniGil2 <86826366+DaniGil2@users.noreply.github.com>
2022-08-10 17:25:04 +0200

    Update baseline_methods_embed.py

--- lib/baseline_methods_embed.py
+++ lib/baseline_methods_embed.py
@@ -29,40 +29,43 @@ def SVM(x_train,y_train,x_test,y_test,dataset):
     svm_models_params = list()
     for gamma in [0.001, 0.01, 0.1, 1, 10, 100]:
         for C in [0.001, 0.01, 0.1, 1, 10, 100]:
-            #Training the SVM for the hyperparam  + preprocessing combinations
-            svm = Pipeline([('clf', SVC(C=C, gamma=gamma))])    
-            svm.fit(x_train, y_train)
-            # evaluate the SVM on the test set
-            score = svm.score(x_test, y_test)
+            for kernel in ['linear', 'rbf', 'sigmoid']:
+                #Training the SVM for the hyperparam  + preprocessing combinations
+                svm = Pipeline([('clf', SVC(C=C, gamma=gamma, kernel=kernel))])    
+                svm.fit(x_train, y_train)
+                # evaluate the SVM on the test set
+                score = svm.score(x_test, y_test)
 
-            #keeping track of the combinations
-            params = {'C': C, 'gamma': gamma}
-            svm_models_params.append((score, params))
+                #keeping track of the combinations
+                params = {'C': C, 'gamma': gamma, 'kernel': kernel}
+                svm_models_params.append((score, params))
 
-            # if we got a better score, store the score and parameters
-            if score > best_svm_score:
-                best_svm_score = score
-                best_svm_parameters = {'C': C, 'gamma': gamma}
-
+                # if we got a better score, store the score and parameters
+                if score > best_svm_score:
+                    best_svm_score = score
+                    best_svm_parameters = {'C': C, 'gamma': gamma, 'kernel': kernel}
+    
     C = best_svm_parameters['C']
     gamma = best_svm_parameters['gamma']
-
-    svm = Pipeline([('clf', SVC(C=C, gamma=gamma))])    
+    kernel = best_svm_parameters['kernel']
+    
+    svm = Pipeline([('clf', SVC(C=C, gamma=gamma, kernel=kernel))])    
     svm.fit(x_train, y_train)
 
     y_test_preds_class = svm.predict(x_test)
     y_test_preds_proba = svm.decision_function(x_test)
 
     new_doc_utils.top2_acc(y_test_preds_proba, y_test, verbose=1)
-
+    
     new_doc_utils.plotConfMatrix(y_test, y_test_preds_class, model="SVM", dataset_type=dataset)
     
+    print("C: " + str(C) + " gamma: " + str(gamma) + " kernel: " + str(kernel))
     
     
 def MNB(x_train,y_train,x_test,y_test,dataset):
     best_mnb_score = 0
     mnb_models_params = list()
-    for alpha in [0.001, 0.01, 0.1, 1]:
+    for alpha in [0, 0.001, 0.01, 0.1, 1 , 2, 5]:
         # for each combination of parameters, train a MNB
         mnb = Pipeline([('clf', MultinomialNB(alpha=alpha))])    
         mnb.fit(x_train, y_train)
@@ -88,4 +91,5 @@ def MNB(x_train,y_train,x_test,y_test,dataset):
     
     new_doc_utils.plotConfMatrix(y_test, y_test_preds_class, model="NB", dataset_type=dataset)
     
+    print("alpha: " + str(alpha))
     
